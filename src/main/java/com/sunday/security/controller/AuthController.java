c82b5c12fe0d2b105a20a714aaf66d4bad9e86d4
user <sunday.peter@decagon.dev>
2024-01-02 16:56:52 +0100

    initial commit

--- src/main/java/com/sunday/security/controller/AuthController.java
+++ src/main/java/com/sunday/security/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.sunday.security.controller;
 
+import com.sunday.security.payload.APIResponse;
+import com.sunday.security.payload.request.adminRequest.AdminRequest;
 import com.sunday.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -10,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping("/register")
+    public ResponseEntity<APIResponse<AdminRequest>> registerAdmin(AdminRequest adminRequest){
+        return authService.registerAdmin(adminRequest);
+    }
 }
